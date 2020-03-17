e7c7a22d27192ce5113be0ef92c565ee67ac791b
PawelJadach <pawel.jadach.96@gmail.com>
2020-03-17 11:44:58 +0100

    add features to tasks

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,78 @@
-import React from 'react';
+import React, { Component } from 'react';
+import io from 'socket.io-client';
 
-function App() {
-  return (
-    <div className="App">
-      Hello world!
-    </div>
-  );
-}
+class App extends Component {
+
+  state = {
+    tasks: [],
+    taskName: '',
+  };
+
+  componentDidMount(){
+    this.socket = io('http://localhost:8000');
+    this.socket.on('updateData', (tasks) => this.updateData(tasks));
+    this.socket.on('addTask', (task) => this.addTask(task));
+    this.socket.on('removeTask', (id) => this.removeTask(id, 'serwer'));
+  };
+
+  updateData(tasks){
+    this.setState((prevState) => ({
+      tasks: tasks,
+    }))
+  }
+
+  removeTask(id, status ='client'){
+    this.setState((prevState) => ({
+      tasks: prevState.tasks.filter((task, index) => index !== id),
+    }));
+    if (status === 'client') this.socket.emit('removeTask', id);
+  };
+
+  submitForm = (e) => {
+    e.preventDefault();
+    const task = this.state.taskName;
+    this.addTask(task);
+    this.setState((prevState) => ({
+      taskName: '',
+    }));
+    this.socket.emit('addTask', task);
+  }
+
+  addTask = (task) => {
+    this.setState((prevState) => ({
+      tasks: prevState.tasks.concat(task),
+    }));
+  }
+
+  onChange = (e) => {
+    this.setState({
+      taskName: e.target.value,
+    });
+  }
+
+  render(){
+    return (
+      <div className="App">
+        <header>
+          <h1>ToDoList.app</h1>
+        </header>
+  
+        <section className="tasks-section" id="tasks-section">
+          <h2>Tasks</h2>
+  
+          <ul className="tasks-section__list" id="tasks-list">
+            {this.state.tasks.map((task, index) => <li key={index} className="task">{task}<button onClick={() => this.removeTask(index)} className="btn btn--red">Remove</button></li>)}
+          </ul>
+  
+          <form id="add-task-form" onSubmit={this.submitForm}>
+            <input onChange={this.onChange} className="text-input" autoComplete="off" type="text" placeholder="Type your description" value={this.state.taskName} id="task-name" />
+            <button className="btn" type="submit">Add</button>
+          </form>
+  
+        </section>
+      </div>
+    );
+  };
+};
 
 export default App;
--- server.js
+++ server.js
@@ -1,11 +1,34 @@
 const express = require('express');
+const socket = require('socket.io');
 
 const app = express();
 const port = process.env.PORT || 8000;
 
-
-const server = app.listen(port, () => console.log(`Server start on port ${port}`));
+const tasks = [];
 
 app.use((req, res) => {
   res.status(404).send({ message: 'Not found...' });
+});
+
+const server = app.listen(port, () => console.log(`Server start on port ${port}`));
+const io = socket(server);
+
+io.on('connection', (socket) => {
+  socket.emit('updateData', tasks);
+  
+  socket.on('addTask', (task) => {
+    tasks.push(task);
+    socket.broadcast.emit('addTask', task);
+  });
+
+  socket.on('removeTask', (index) => {
+    tasks.splice(index, 1);
+    socket.broadcast.emit('removeTask', index);
+  });
+
+  // socket.on('disconnect', () => {
+  //   const user =  users[users.findIndex(user => user.id === socket.id)];
+  //   io.emit('message', {author: 'Chat Bot', content: `${user.userName} has left the conversation... :(`});
+  //   users.splice(users.findIndex(user => user.id === socket.id), 1);
+  // });
 });
